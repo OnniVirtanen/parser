d148d5c60d439e9515517ff4fddab6f41e159073
Onni Virtanen <onni.virtanen5@gmail.com>
2024-07-09 15:03:47 +0300

    made eof token final

--- parser/src/main/java/org/example/token/eof/EndOfFileToken.java
+++ parser/src/main/java/org/example/token/eof/EndOfFileToken.java
@@ -3,7 +3,7 @@ package org.example.token.eof;
 import org.example.token.Token;
 import org.example.token.TokenType;
 
-public class EndOfFileToken extends Token {
+public final class EndOfFileToken extends Token {
 
     public EndOfFileToken() {
         super(TokenType.EOF);
